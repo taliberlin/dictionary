ed79075273bc0d015c553c86a5049c07231a6031
taliberlin <118308290+taliberlin@users.noreply.github.com>
2023-08-07 21:50:58 -0700

    implemented photo API

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -6,6 +6,7 @@ import Results from "./Results";
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
   let [results, setResults] = useState(null);
+  let [photos, setPhotos] = useState(null);
 
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
@@ -15,11 +16,21 @@ export default function Dictionary() {
     setResults(response.data);
   }
 
+  function getPhotoResponse(response) {
+    console.log(response);
+  }
+
   function search(event) {
     event.preventDefault();
     let apiKey = "o599f3bbe3f722tbacc3ebf3032624a0";
     let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
     axios.get(apiUrl).then(getResponse);
+
+    let photoApiKey =
+      "bxPMt0l6zNnwvWYS8hxcMjmhy14OKhHS4qfoGeoEgX7yIpTwLYpkGCOg";
+    let photoApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=1`;
+    let headers = { Authorization: `Bearer ${photoApiKey}` };
+    axios.get(photoApiUrl, { headers: headers }).then(getPhotoResponse);
   }
 
   return (
